a2417dfed9963f4ee4d087137c897b7a34862527
dvinubius <dvinubiu.narmon@gmail.com>
2024-08-05 19:31:16 +0300

    simplify example

--- build/output.lua
+++ build/output.lua
@@ -40,19 +40,3 @@ end
 
 local dep1 = require("dep1")
 local dep2 = require("dep2")
-
-Handlers.add(
-  'test',
-  Handlers.utils.hasMatchingTag('Action', 'Test'),
-  function()
-    print("IN test")
-    print("Calling dep1.foo")
-    dep1.foo()
-    print("Calling dep1.bar")
-    dep1.bar()
-    print("Calling dep2.foo")
-    dep2.foo()
-    print("Calling dep2.bar")
-    dep2.bar()
-  end
-)
